50e327e45f49cbc352d168296c8320ccab30ac0b
Gabriel Badaró <grbadas@gmail.com>
2023-03-30 16:02:57 -0300

    working

--- src/main/java/com/badas/springboot/DTO/UserLoginDTO.java
+++ src/main/java/com/badas/springboot/DTO/UserLoginDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class UserLoginDTO {
 	
-	private String name;
+	private String Username;
 	
 	private long otp;
 
--- src/main/java/com/badas/springboot/controller/LoginController.java
+++ src/main/java/com/badas/springboot/controller/LoginController.java
@@ -1,13 +1,39 @@
 package com.badas.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+import com.badas.springboot.DTO.UserLoginDTO;
+import com.badas.springboot.repository.UserModelRepository;
+import com.badas.springboot.service.DefaultUserService;
+
+@Controller
+@RequestMapping("/login")
 public class LoginController {
- 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+	@Autowired
+	private DefaultUserService userService;
+	
+	@Autowired
+	UserModelRepository userRepo;
+    
+    @ModelAttribute("user")
+    public UserLoginDTO userLoginDTO() {
+        return new UserLoginDTO();
     }
+    
+	@GetMapping
+	public String login() {
+		return "login";
+	}
+	
+	@PostMapping
+	public void loginUser(@ModelAttribute("user") 
+	UserLoginDTO userLoginDTO) {
+		System.out.println("UserDTO"+userLoginDTO);
+		 userService.loadUserByUsername(userLoginDTO.getUsername());
+	}
 }
